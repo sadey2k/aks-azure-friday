0c2510435a21ac5c42d932c7895c49d708a64385
Shola Ade <sadey2k@yahoo.com>
2022-06-09 23:02:34 +0100

    storage account updated

--- provider.tf
+++ provider.tf
@@ -16,10 +16,10 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     resource_group_name  = "aks-storage-RG-uk west"
-    storage_account_name = "tfstate9400"
+    storage_account_name = "tfstate8018"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
 
-    sas_token = "?sv=2017-07-29&ss=b&srt=sco&sp=rwdlac&se=2024-06-07T10:54:55Z&st=2022-06-08T10:54:55Z&spr=https&sig=9G2HkooEz8nGfY4EmazVwwgFH5wrlGDAf18WK3Po6fk%3D"
+    sas_token = "?sv=2017-07-29&ss=b&srt=sco&sp=rwdlac&se=2024-06-08T21:55:43Z&st=2022-06-09T21:55:43Z&spr=https&sig=hEblXAjrtvFsj%2Bwa3uuo9opKB18Tuj%2BcKHlvkGkPHtY%3D"
   }
 }
