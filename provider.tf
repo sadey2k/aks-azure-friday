8e133fa0af0e9c2c50b2da6194708b8119d28e3d
Shola Ade <sadey2k@yahoo.com>
2022-06-10 00:32:46 +0100

    resource group name updated5

--- provider.tf
+++ provider.tf
@@ -13,13 +13,13 @@ provider "azurerm" {
 
 
 ## Terraform State State Storage ##
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "aks-storage-RG"
-#     storage_account_name = "tfstate8018"
-#     container_name       = "tfstate"
-#     key                  = "terraform.tfstate"
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "aks-storage-RG"
+    storage_account_name = "tfstate8018"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
 
-#     sas_token = "?sv=2017-07-29&ss=b&srt=sco&sp=rwdlac&se=2024-06-08T21:55:43Z&st=2022-06-09T21:55:43Z&spr=https&sig=hEblXAjrtvFsj%2Bwa3uuo9opKB18Tuj%2BcKHlvkGkPHtY%3D"
-#   }
-# }
+    sas_token = "?sv=2017-07-29&ss=b&srt=sco&sp=rwdlac&se=2024-06-08T21:55:43Z&st=2022-06-09T21:55:43Z&spr=https&sig=hEblXAjrtvFsj%2Bwa3uuo9opKB18Tuj%2BcKHlvkGkPHtY%3D"
+  }
+}
